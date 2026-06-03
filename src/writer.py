79e51038462a46faca966303fd70cd4c796191c7
deveduar <deveduar@gmail.com>
2026-06-03 22:14:59 +0200

    audio-vosk: fix writer init, debug mode

--- src/writer.py
+++ src/writer.py
@@ -1,38 +1,184 @@
 import ctypes
+import logging
 import sys
+import time
+from ctypes import wintypes
+
+logger = logging.getLogger("writer")
+handler = logging.FileHandler("writer_debug.log", mode="w", encoding="utf-8")
+handler.setFormatter(logging.Formatter("%(asctime)s %(message)s"))
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
+VK_CONTROL = 0x11
+VK_V = 0x56
+KEYEVENTF_KEYDOWN = 0
+KEYEVENTF_KEYUP = 2
+KEYEVENTF_UNICODE = 0x0004
+INPUT_KEYBOARD = 1
+CF_UNICODETEXT = 13
+GMEM_MOVABLE = 0x0002
+GMEM_ZEROINIT = 0x0040
+
+
+class KEYBDINPUT(ctypes.Structure):
+    _fields_ = [
+        ("wVk", wintypes.WORD),
+        ("wScan", wintypes.WORD),
+        ("dwFlags", wintypes.DWORD),
+        ("time", wintypes.DWORD),
+        ("dwExtraInfo", ctypes.c_void_p),
+    ]
+
+
+class MOUSEINPUT(ctypes.Structure):
+    _fields_ = [
+        ("dx", wintypes.LONG),
+        ("dy", wintypes.LONG),
+        ("mouseData", wintypes.DWORD),
+        ("dwFlags", wintypes.DWORD),
+        ("time", wintypes.DWORD),
+        ("dwExtraInfo", ctypes.c_void_p),
+    ]
+
+
+class HARDWAREINPUT(ctypes.Structure):
+    _fields_ = [
+        ("uMsg", wintypes.DWORD),
+        ("wParamL", wintypes.WORD),
+        ("wParamH", wintypes.WORD),
+    ]
+
+
+class _INPUT_UNION(ctypes.Union):
+    _fields_ = [
+        ("mi", MOUSEINPUT),
+        ("ki", KEYBDINPUT),
+        ("hi", HARDWAREINPUT),
+    ]
+
+
+class INPUT(ctypes.Structure):
+    _fields_ = [
+        ("type", wintypes.DWORD),
+        ("union", _INPUT_UNION),
+    ]
+
+
+user32 = ctypes.windll.user32
+kernel32 = ctypes.windll.kernel32
+
+user32.SendInput.argtypes = [wintypes.UINT, ctypes.c_void_p, ctypes.c_int]
+user32.SendInput.restype = wintypes.UINT
+user32.GetForegroundWindow.argtypes = []
+user32.GetForegroundWindow.restype = wintypes.HWND
+user32.GetWindowTextW.argtypes = [wintypes.HWND, ctypes.c_wchar_p, ctypes.c_int]
+user32.GetWindowTextW.restype = ctypes.c_int
+user32.GetClassNameW.argtypes = [wintypes.HWND, ctypes.c_wchar_p, ctypes.c_int]
+user32.GetClassNameW.restype = ctypes.c_int
+kernel32.GetLastError.argtypes = []
+kernel32.GetLastError.restype = wintypes.DWORD
+
+user32.OpenClipboard.argtypes = [wintypes.HANDLE]
+user32.OpenClipboard.restype = wintypes.BOOL
+user32.EmptyClipboard.argtypes = []
+user32.EmptyClipboard.restype = wintypes.BOOL
+user32.CloseClipboard.argtypes = []
+user32.CloseClipboard.restype = wintypes.BOOL
+user32.SetClipboardData.argtypes = [wintypes.UINT, wintypes.HANDLE]
+user32.SetClipboardData.restype = wintypes.HANDLE
+
+kernel32.GlobalAlloc.argtypes = [wintypes.UINT, ctypes.c_size_t]
+kernel32.GlobalAlloc.restype = wintypes.HANDLE
+kernel32.GlobalLock.argtypes = [wintypes.HANDLE]
+kernel32.GlobalLock.restype = wintypes.LPVOID
+kernel32.GlobalUnlock.argtypes = [wintypes.HANDLE]
+kernel32.GlobalUnlock.restype = wintypes.BOOL
+kernel32.GlobalFree.argtypes = [wintypes.HANDLE]
+kernel32.GlobalFree.restype = wintypes.HANDLE
+
+
+def _debug_foreground():
+    hwnd = user32.GetForegroundWindow()
+    title = ctypes.c_wchar_p(" " * 256)
+    cls = ctypes.c_wchar_p(" " * 256)
+    user32.GetWindowTextW(hwnd, title, 256)
+    user32.GetClassNameW(hwnd, cls, 256)
+    logger.debug("foreground hwnd=%s class=%s title=%s", hwnd, cls.value, title.value)
+    return hwnd
+
+
+def _send_input(inp):
+    ret = user32.SendInput(1, ctypes.byref(inp), ctypes.sizeof(inp))
+    err = kernel32.GetLastError()
+    if ret == 0:
+        logger.debug("SendInput FAILED ret=%d err=%d", ret, err)
+    else:
+        logger.debug("SendInput OK ret=%d err=%d", ret, err)
+    return ret
+
+
+def _send_unicode(ch):
+    code = ord(ch)
+    inp_down = INPUT(INPUT_KEYBOARD, _INPUT_UNION(ki=KEYBDINPUT(0, code, KEYEVENTF_UNICODE, 0, 0)))
+    inp_up = INPUT(INPUT_KEYBOARD, _INPUT_UNION(ki=KEYBDINPUT(0, code, KEYEVENTF_UNICODE | KEYEVENTF_KEYUP, 0, 0)))
+    _send_input(inp_down)
+    _send_input(inp_up)
+
+
+def _paste_clipboard():
+    inp_down_ctl = INPUT(INPUT_KEYBOARD, _INPUT_UNION(ki=KEYBDINPUT(VK_CONTROL, 0, KEYEVENTF_KEYDOWN, 0, 0)))
+    inp_down_v = INPUT(INPUT_KEYBOARD, _INPUT_UNION(ki=KEYBDINPUT(VK_V, 0, KEYEVENTF_KEYDOWN, 0, 0)))
+    inp_up_v = INPUT(INPUT_KEYBOARD, _INPUT_UNION(ki=KEYBDINPUT(VK_V, 0, KEYEVENTF_KEYUP, 0, 0)))
+    inp_up_ctl = INPUT(INPUT_KEYBOARD, _INPUT_UNION(ki=KEYBDINPUT(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0, 0)))
+    for inp in (inp_down_ctl, inp_down_v, inp_up_v, inp_up_ctl):
+        _send_input(inp)
+
+
+def _set_clipboard(text):
+    for attempt in range(3):
+        if user32.OpenClipboard(None):
+            break
+        time.sleep(0.05)
+    else:
+        logger.debug("clipboard OpenClipboard failed after 3 attempts")
+        return False
+    try:
+        user32.EmptyClipboard()
+        text_bytes = (text + "\0").encode("utf-16-le")
+        h = kernel32.GlobalAlloc(GMEM_MOVABLE | GMEM_ZEROINIT, len(text_bytes))
+        if not h:
+            logger.debug("clipboard GlobalAlloc failed")
+            return False
+        p = kernel32.GlobalLock(h)
+        if not p:
+            logger.debug("clipboard GlobalLock failed")
+            kernel32.GlobalFree(h)
+            return False
+        ctypes.memmove(p, text_bytes, len(text_bytes))
+        kernel32.GlobalUnlock(h)
+        user32.SetClipboardData(CF_UNICODETEXT, h)
+        logger.debug("clipboard OK wrote %d chars", len(text))
+    finally:
+        user32.CloseClipboard()
+    return True
 
 
 def write_text(text):
     if sys.platform != "win32":
+        logger.debug("write_text not win32, skip")
         return False
 
-    user32 = ctypes.windll.user32
-    kernel32 = ctypes.windll.kernel32
+    logger.debug("write_text text='%s' len=%d", text[:50], len(text))
+    logger.debug("write_text sizeof(INPUT)=%d", ctypes.sizeof(INPUT))
 
-    user32.OpenClipboard(None)
-    user32.EmptyClipboard()
+    hwnd = _debug_foreground()
 
-    text_bytes = (text + "\0").encode("utf-16-le")
-    h = kernel32.GlobalAlloc(0x2000, len(text_bytes))
-    if not h:
-        return False
-    p = kernel32.GlobalLock(h)
-    if not p:
-        kernel32.GlobalFree(h)
-        return False
-    ctypes.memmove(p, text_bytes, len(text_bytes))
-    kernel32.GlobalUnlock(h)
-    user32.SetClipboardData(13, h)
-    user32.CloseClipboard()
+    for i, ch in enumerate(text):
+        _send_unicode(ch)
+        if i == 0:
+            logger.debug("write_text first char sent, re-checking foreground...")
+            _debug_foreground()
 
-    KEYEVENTF_KEYDOWN = 0
-    KEYEVENTF_KEYUP = 2
-    VK_CONTROL = 0x11
-    VK_V = 0x56
-
-    user32.keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYDOWN, 0)
-    user32.keybd_event(VK_V, 0, KEYEVENTF_KEYDOWN, 0)
-    user32.keybd_event(VK_V, 0, KEYEVENTF_KEYUP, 0)
-    user32.keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0)
-
+    logger.debug("write_text done")
     return True
